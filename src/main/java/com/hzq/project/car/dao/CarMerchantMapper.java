e58562843d9627660ba25db9aff2f8d78069bbce
Your Name <you@example.com>
2016-09-25 20:05:44 +0800

    commit

---
+++ src/main/java/com/hzq/project/car/dao/CarMerchantMapper.java
@@ -0,0 +1,8 @@
+package com.hzq.project.car.dao;
+
+import com.hzq.project.car.dao.entity.CarMerchant;
+import com.hzq.project.system.common.dao.Dao;
+
+public interface CarMerchantMapper extends Dao<CarMerchant,Integer> {
+    CarMerchant getCarMerchantByUserId(Integer userId);
+}
---
+++ src/main/java/com/hzq/project/car/dao/entity/CarMerchant.java
@@ -0,0 +1,135 @@
+package com.hzq.project.car.dao.entity;
+
+import java.util.Date;
+
+public class CarMerchant {
+    private Integer id;
+
+    private String company;
+
+    private Integer type;
+
+    private Integer userId;
+
+    private String name;
+
+    private String phone;
+
+    private String picture;
+
+    private String idCard;
+
+    private Integer province;
+
+    private String address;
+
+    private Integer watch;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public Integer getProvince() {
+        return province;
+    }
+
+    public void setProvince(Integer province) {
+        this.province = province;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getWatch() {
+        return watch;
+    }
+
+    public void setWatch(Integer watch) {
+        this.watch = watch;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
