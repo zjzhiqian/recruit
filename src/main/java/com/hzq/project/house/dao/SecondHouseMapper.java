e0d489804e0349c0c1bc3c23ec1e838f40986799
Your Name <you@example.com>
2016-10-16 11:46:16 +0800

    提交

---
+++ src/main/java/com/hzq/project/house/dao/SecondHouseMapper.java
@@ -0,0 +1,7 @@
+package com.hzq.project.house.dao;
+
+import com.hzq.project.house.dao.entity.SecondHouse;
+import com.hzq.project.system.common.dao.Dao;
+
+public interface SecondHouseMapper extends Dao<SecondHouse,Integer> {
+}
---
+++ src/main/java/com/hzq/project/house/dao/entity/SecondHouse.java
@@ -0,0 +1,236 @@
+package com.hzq.project.house.dao.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class SecondHouse {
+    private Integer id;
+
+    private Integer userId;
+
+    private Integer area;
+
+    private String address;
+
+    private Integer shi;
+
+    private Integer ting;
+
+    private Integer wei;
+
+    private Integer big;
+
+    private String lou;
+
+    private Integer ceng;
+
+    private String toward;
+
+    private String decorate;
+
+    private String type;
+
+    private BigDecimal price;
+
+    private String title;
+
+    private String introduction;
+
+    private String carPic;
+
+    private String concactName;
+
+    private String concactPhone;
+
+    private Integer watch;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    private Date deletedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getArea() {
+        return area;
+    }
+
+    public void setArea(Integer area) {
+        this.area = area;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getShi() {
+        return shi;
+    }
+
+    public void setShi(Integer shi) {
+        this.shi = shi;
+    }
+
+    public Integer getTing() {
+        return ting;
+    }
+
+    public void setTing(Integer ting) {
+        this.ting = ting;
+    }
+
+    public Integer getWei() {
+        return wei;
+    }
+
+    public void setWei(Integer wei) {
+        this.wei = wei;
+    }
+
+    public Integer getBig() {
+        return big;
+    }
+
+    public void setBig(Integer big) {
+        this.big = big;
+    }
+
+    public String getLou() {
+        return lou;
+    }
+
+    public void setLou(String lou) {
+        this.lou = lou;
+    }
+
+    public Integer getCeng() {
+        return ceng;
+    }
+
+    public void setCeng(Integer ceng) {
+        this.ceng = ceng;
+    }
+
+    public String getToward() {
+        return toward;
+    }
+
+    public void setToward(String toward) {
+        this.toward = toward;
+    }
+
+    public String getDecorate() {
+        return decorate;
+    }
+
+    public void setDecorate(String decorate) {
+        this.decorate = decorate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public String getCarPic() {
+        return carPic;
+    }
+
+    public void setCarPic(String carPic) {
+        this.carPic = carPic;
+    }
+
+    public String getConcactName() {
+        return concactName;
+    }
+
+    public void setConcactName(String concactName) {
+        this.concactName = concactName;
+    }
+
+    public String getConcactPhone() {
+        return concactPhone;
+    }
+
+    public void setConcactPhone(String concactPhone) {
+        this.concactPhone = concactPhone;
+    }
+
+    public Integer getWatch() {
+        return watch;
+    }
+
+    public void setWatch(Integer watch) {
+        this.watch = watch;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Date getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Date deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+}
